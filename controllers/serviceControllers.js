0d73b04805df6f7aecde4c42fda08311ac915d7a
daniel-vns <daniel.vns@live.com>
2023-11-17 16:15:47 -0300

    Service CRUD completed

--- controllers/serviceControllers.js
+++ controllers/serviceControllers.js
@@ -1,5 +1,67 @@
 const { Service: ServiceModel } = require("../models/Service");
 
-const serviceController = {};
+const serviceController = {
+  create: async (req, res) => {
+    try {
+      const service = {
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+      };
+      const response = await ServiceModel.create(service);
+      res
+        .status(201)
+        .json({ response, message: "Service created successfully!!!" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  getAll: async (req, res) => {
+    try {
+      const services = await ServiceModel.find();
+      res.json(services);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  get: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const service = await ServiceModel.findById(id);
+      res.json(service);
+    } catch (error) {
+      res.status(404).json({ message: "Service not found!!!" });
+    }
+  },
+
+  delete: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const deletedService = await ServiceModel.findByIdAndDelete(id);
+      res
+        .status(200)
+        .json({ deletedService, message: "Service deleted successfuly!!!" });
+    } catch (error) {
+      res.status(404).json({ message: "Service not found!!!" });
+    }
+  },
+
+  update: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const service = {
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+      };
+      const updatedService = await ServiceModel.findByIdAndUpdate(id, service);
+      res.status(200).json({ service, message: "Service updated!!!" });
+    } catch (error) {
+      res.status(404).json({ message: "Service not found!!!" });
+    }
+  },
+};
 
 module.exports = serviceController;
--- routes/services.js
+++ routes/services.js
@@ -1,5 +1,23 @@
+const router = require("express").Router();
+
 const serviceController = require("../controllers/serviceControllers");
 
-const router = require("express").Router();
+router
+  .route("/services")
+  .post((req, res) => serviceController.create(req, res));
+
+router.route("/services").get((req, res) => serviceController.getAll(req, res));
+
+router
+  .route("/services/:id")
+  .get((req, res) => serviceController.get(req, res));
+
+router
+  .route("/services/:id")
+  .delete((req, res) => serviceController.delete(req, res));
+
+router
+  .route("/services/:id")
+  .put((req, res) => serviceController.update(req, res));
 
 module.exports = router;
